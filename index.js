46ae8c62ca79ea5df1ea6ec63a4fee1d332195cf
abdul basit <abdulbasit2745@gmail.com>
2025-07-30 16:49:08 +0500

    commit 1

---
+++ index.js
@@ -0,0 +1,2 @@
+"use strict";
+console.log("my name is abdul basit and i ama a student of generative and agentic AI");
---
+++ index.ts
@@ -0,0 +1,1 @@
+console.log ("my name is abdul basit and i ama a student of generative and agentic AI");
