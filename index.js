ce3473177930556cb3c770bb5605a4d4e5e05038
abdul basit <abdulbasit2745@gmail.com>
2025-08-03 15:43:49 +0500

    change

--- index.js
+++ index.js
@@ -1,2 +1,2 @@
 "use strict";
-console.log("my name is abdul basit and i ama a student of generative and agentic AI");
+console.log("my name is abdul basit and i am a student of generative and agentic AI");
--- index.ts
+++ index.ts
@@ -1,1 +1,1 @@
-console.log ("my name is abdul basit and i ama a student of generative and agentic AI");
+console.log ("my name is abdul basit and i am a student of generative and agentic AI");
